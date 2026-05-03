a9e2e6af38c8c78d33145e87ba3792589a36dd1c
Rozana <rozanashaqqou74@gmail.com>
2026-05-03 17:57:22 +0300

    feat(config): update database configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,9 +10,22 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
+    forbidNonWhitelisted: true,
   }));
 
+    // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Wasal API')
+    .setDescription('Wessal platform — managing donations and coupons')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
+
+
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
